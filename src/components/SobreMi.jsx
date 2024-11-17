df14b77d501e492379cb35047b0fcde155da2926
Hamid Freig <110936554+HamidFreig@users.noreply.github.com>
2024-11-17 18:03:13 -0300

    termino de seccion - sobre mi

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,36 +1,77 @@
 // src/components/Section3.js
 import React from "react";
+import {
+  FaReact,
+  FaNodeJs,
+  FaJsSquare,
+  FaDatabase,
+  FaGitAlt,
+  FaJava,
+} from "react-icons/fa";
+import { DiCss3, DiHtml5 } from "react-icons/di";
+import { SiFirebase, SiPython, SiPowerbi } from "react-icons/si";
 
 const Section3 = () => {
+  const skills = [
+    { name: "React", icon: <FaReact className="text-4xl text-blue-500" /> },
+    { name: "Node.js", icon: <FaNodeJs className="text-4xl text-green-500" /> },
+    {
+      name: "JavaScript",
+      icon: <FaJsSquare className="text-4xl text-yellow-500" />,
+    },
+    {
+      name: "Java",
+      icon: <FaJava className="text-4xl text-white-500" />,
+    },
+    { name: "MySQL", icon: <FaDatabase className="text-4xl text-gray-500" /> },
+    {
+      name: "Git/GitHub",
+      icon: <FaGitAlt className="text-4xl text-red-500" />,
+    },
+    { name: "CSS", icon: <DiCss3 className="text-4xl text-blue-600" /> },
+    { name: "HTML", icon: <DiHtml5 className="text-4xl text-orange-500" /> },
+    {
+      name: "Firebase",
+      icon: <SiFirebase className="text-4xl text-yellow-500" />,
+    },
+    { name: "Python", icon: <SiPython className="text-4xl text-blue-500" /> },
+    {
+      name: "Power BI",
+      icon: <SiPowerbi className="text-4xl text-yellow-600" />,
+    },
+  ];
+
   return (
-    <section id="seccion2" className="h-screen text-white p-4">
+    <section id="seccion2" className="h-screen text-white p-4 w-full">
       {/* Grid con 2 columnas principales */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-center p-4">
         {/* Columna izquierda */}
-        <div className="flex flex-col justify-between space-y-8">
+        <div className="flex flex-col justify-between space-y-2">
           {/* Título Sobre Mí */}
-          <div>
-            <p className="text-xl lg:text-4xl font-bold mb-4">
+          <div className="p-4">
+            <p className="text-xl lg:text-4xl font-bold mb-4 text-center   ">
               &lt; Sobre Mí /&gt;
             </p>
-            <p className="text-md text-justify">
-              Soy un profesional con estudios en ingeniería informática y
-              experiencia en consultoría empresarial. Me he especializado en el
-              análisis y optimización de procesos empresariales, con un enfoque
-              en la implementación de soluciones tecnológicas y la gestión de
-              proyectos. Me apasiona ayudar a las empresas a mejorar su
-              eficiencia y adaptarse a los desafíos del entorno digital.
+            <p className="text-md text-justify border-t-4 p-2 font-sans font-semibold">
+              Ingeniero Civil Informático egresado de la Universidad Andrés
+              Bello, con residencia en Santiago, Chile. A lo largo de mi
+              formación y experiencia profesional, me he destacado por ser una
+              persona altamente responsable, proactiva y con una clara
+              orientación hacia el cumplimiento de objetivos. Mi motivación
+              constante por afrontar nuevos desafíos me impulsa a mejorar de
+              manera continua y a buscar soluciones innovadoras que generen
+              valor.
             </p>
           </div>
 
           {/* Título Experiencia */}
-          <div>
-            <p className="text-xl lg:text-4xl font-bold mb-4">
+          <div className="p-4 font-sans font-semibold">
+            <p className="text-xl lg:text-4xl font-bold mb-4 text-center border-b-4 p-2">
               &lt; Experiencia /&gt;
             </p>
             <ul className="flex-col">
               <li className="w-full ">
-                <p className="text-lg font-bold">Business Consultant</p>
+                <p className="text-lg font-bold ">Business Consultant</p>
                 <p className="text-md font-bold">
                   Septiembre 2023 - Actualidad
                 </p>
@@ -83,19 +124,19 @@ const Section3 = () => {
         {/* Columna derecha: Skills */}
         <div className="flex flex-col justify-between space-y-8">
           {/* Título Skills */}
-          <div>
-            <p className="text-xl lg:text-4xl font-bold mb-4">
-              &lt; Skills /&gt;
-            </p>
-            <ul className="text-lg space-y-4">
-              <li>React</li>
-              <li>Node.js</li>
-              <li>NestJS</li>
-              <li>MySQL</li>
-              <li>JavaScript</li>
-              <li>CSS/Tailwind CSS</li>
-              <li>Git/GitHub</li>
-            </ul>
+          <p className="text-xl lg:text-4xl font-bold mb-4 text-center border-b-4 p-2">
+            &lt; Skills /&gt;
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+            {skills.map((skill, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center p-4 border border-gray-300 rounded-lg shadow-lg hover:shadow-xl transition-all duration-300"
+              >
+                <div className="mb-4">{skill.icon}</div>
+                <h3 className="text-xl font-semibold">{skill.name}</h3>
+              </div>
+            ))}
           </div>
         </div>
       </div>
